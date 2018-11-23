deea1fa4c93ae0c079b480c4cfa634eab0820e95
LCLY <lchoo1996@gmail.com>
2018-11-23 03:52:29 -0500

    feat: added sticky navbar

---
+++ web/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(".navbar").hide();
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$(".navbar").fadeIn(500);
+		} else {
+			$(".navbar").fadeOut(500);
+		}
+	});
+});
